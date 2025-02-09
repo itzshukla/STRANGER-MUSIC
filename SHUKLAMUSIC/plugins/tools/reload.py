a83fb8e36caeb78cf1342694bb419007691800d1
STRANGER <147219905+itzshukla@users.noreply.github.com>
2025-02-10 01:07:54 +0530

    Update reload.py

--- SHUKLAMUSIC/plugins/tools/reload.py
+++ SHUKLAMUSIC/plugins/tools/reload.py
@@ -109,7 +109,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7795453980)
+    & filters.user(6919199044)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
