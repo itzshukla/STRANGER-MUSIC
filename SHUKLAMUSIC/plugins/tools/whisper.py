97096ccaff7d770475a997baf8c0c4e8bf0389a0
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-20 17:06:20 +0530

    Update whisper.py

--- SHUKLAMUSIC/plugins/tools/whisper.py
+++ SHUKLAMUSIC/plugins/tools/whisper.py
@@ -110,8 +110,8 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="💒 Whisper",
-            description=f"@Itz_SapnaMusicbot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@Itz_SapnaMusicbot (Target Username or ID) (Your Message).\n\n**Example:**\n@Itz_SapnaMusicbot @username I Wanna Phuck You"),
+            description=f"@SapnaMusicRobot [USERNAME | ID] [TEXT]",
+            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@SapnaMusicRobot (Target Username or ID) (Your Message).\n\n**Example:**\n@SapnaMusicRobot @username I Wanna Phuck You"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
         )
