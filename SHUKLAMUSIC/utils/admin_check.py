46e01eb3d3f837618fc38e63e291b85c563268c2
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-29 16:17:06 +0530

    Update admin_check.py

--- SHUKLAMUSIC/utils/admin_check.py
+++ SHUKLAMUSIC/utils/admin_check.py
@@ -38,3 +38,29 @@ async def is_group_owner(message_or_cq) -> bool:
     client = message._client
     member = await client.get_chat_member(message.chat.id, message.from_user.id)
     return member.status == ChatMemberStatus.OWNER
+
+async def admin_check(message: Message) -> bool:
+    if not message.from_user:
+        return False
+
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
+        return False
+
+    if message.from_user.id in [
+        777000,  # Telegram Service Notifications
+        1087968824,  # GroupAnonymousBot
+    ]:
+        return True
+
+    client = message._client
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    check_status = await client.get_chat_member(chat_id=chat_id, user_id=user_id)
+    if check_status.status not in [
+        ChatMemberStatus.OWNER,
+        ChatMemberStatus.ADMINISTRATOR
+    ]:
+        return False
+    else:
+        return True
