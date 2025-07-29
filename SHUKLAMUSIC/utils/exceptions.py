5f5282f2906b725137fd7a251b316829e4da2098
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-29 16:40:10 +0530

    Update exceptions.py

--- SHUKLAMUSIC/utils/exceptions.py
+++ SHUKLAMUSIC/utils/exceptions.py
@@ -1,3 +1,27 @@
+from typing import Union
+
 class AssistantErr(Exception):
     def __init__(self, errr: str):
         super().__init__(errr)
+
+IGNORED_ERROR_KEYWORDS = [
+    "Nᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ғᴏᴜɴᴅ",
+    "Please start videochat",
+]
+
+IGNORED_EXCEPTION_CLASSES = (
+    # AssistantErr,  # ❌ Currently disabled
+)
+
+def is_ignored_error(err: Union[Exception, BaseException]) -> bool:
+    """
+    Determine if the error should be skipped from full logging.
+    Matches:
+    - Exception type (if enabled)
+    - Known substrings in error message
+    """
+    if isinstance(err, IGNORED_EXCEPTION_CLASSES):
+        return True
+
+    err_str = str(err).lower()
+    return any(keyword.lower() in err_str for keyword in IGNORED_ERROR_KEYWORDS)
