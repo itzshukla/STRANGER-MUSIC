30d02f7b365fe2f1dcd4264f579f261da7d5e178
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-29 16:13:52 +0530

    Update admin_check.py

--- SHUKLAMUSIC/utils/admin_check.py
+++ SHUKLAMUSIC/utils/admin_check.py
@@ -1,29 +1,40 @@
-from pyrogram.types import Message
+from pyrogram.types import CallbackQuery
 from pyrogram.enums import ChatType, ChatMemberStatus
 
+async def is_admin(message_or_cq) -> bool:
+    if isinstance(message_or_cq, CallbackQuery):
+        message = message_or_cq.message
+    else:
+        message = message_or_cq
 
-async def admin_check(message: Message) -> bool:
     if not message.from_user:
         return False
 
-    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL, ChatType.GROUP]:
         return False
 
-    if message.from_user.id in [
-        777000,  # Telegram Service Notifications
-        1087968824,  # GroupAnonymousBot
-    ]:
+    if message.from_user.id in [777000, 1087968824]:
         return True
 
     client = message._client
-    chat_id = message.chat.id
-    user_id = message.from_user.id
+    member = await client.get_chat_member(message.chat.id, message.from_user.id)
+    return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]
 
-    check_status = await client.get_chat_member(chat_id=chat_id, user_id=user_id)
-    if check_status.status not in [
-        ChatMemberStatus.OWNER,
-        ChatMemberStatus.ADMINISTRATOR
-    ]:
-        return False
+async def is_group_owner(message_or_cq) -> bool:
+    if isinstance(message_or_cq, CallbackQuery):
+        message = message_or_cq.message
     else:
+        message = message_or_cq
+
+    if not message.from_user:
+        return False
+
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL, ChatType.GROUP]:
+        return False
+
+    if message.from_user.id in [777000, 1087968824]:
         return True
+
+    client = message._client
+    member = await client.get_chat_member(message.chat.id, message.from_user.id)
+    return member.status == ChatMemberStatus.OWNER
