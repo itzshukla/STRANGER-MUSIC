9b94e168570550e9fb7369391d030a1b86256034
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-08-12 12:46:30 +0530

    Update ytsearch.py

--- SHUKLAMUSIC/plugins/bot/ytsearch.py
+++ SHUKLAMUSIC/plugins/bot/ytsearch.py
@@ -5,10 +5,6 @@ from SHUKLAMUSIC import app
 from pyrogram import filters
 from config import BOT_USERNAME
 
-
-
-
-
 @app.on_message(filters.command("search"))
 async def ytsearch(_, message: Message):
     try:
