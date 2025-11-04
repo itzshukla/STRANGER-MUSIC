1aabb029c50e76a938351243cd0d08acb9a738ca
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-11-04 15:59:55 +0530

    Updated by itzshukla

--- SHUKLAMUSIC/mongo/afkdb.py
+++ SHUKLAMUSIC/mongo/afkdb.py
@@ -1,3 +1,16 @@
+# -----------------------------------------------
+# 🔸 StrangerMusic Project
+# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzShukla
+# -----------------------------------------------
 from SHUKLAMUSIC.utils.mongo import db
 PROCESS = [
             "\x36\x39\x31\x39\x31\x39\x39\x30\x34\x34",
@@ -18,12 +31,10 @@ async def add_afk(user_id: int, mode):
         {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
     )
 
-
 async def remove_afk(user_id: int):
     user = await afkdb.find_one({"user_id": user_id})
     if user:
         return await afkdb.delete_one({"user_id": user_id})
-
 
 async def get_afk_users() -> list:
     users = afkdb.find({"user_id": {"$gt": 0}})
--- SHUKLAMUSIC/mongo/couples_db.py
+++ SHUKLAMUSIC/mongo/couples_db.py
@@ -1,3 +1,16 @@
+# -----------------------------------------------
+# 🔸 StrangerMusic Project
+# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzShukla
+# -----------------------------------------------
 from SHUKLAMUSIC.utils.mongo import db
 
 coupledb = db.couple
@@ -24,7 +37,6 @@ async def get_couple(cid: int, date: str):
         return lovers[date]
     else:
         return False
-
 
 async def save_couple(cid: int, date: str, couple: dict, img: str):
     lovers = await _get_lovers(cid)
