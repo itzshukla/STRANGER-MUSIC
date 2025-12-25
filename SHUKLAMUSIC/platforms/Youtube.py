76af610548400f980bd2880015eecd613a7ca486
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-12-25 18:00:26 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -31,8 +31,8 @@ import aiohttp
 from SHUKLAMUSIC import LOGGER
 from urllib.parse import urlparse
 
-API_BASE_URL = "http://165.22.220.188:8000"
-API_KEY = os.getenv("YT_API_KEY", "strangerapi95df51dde")
+API_BASE_URL = "http://139.59.31.247:8080"
+API_KEY = os.getenv("API_KEY", "")
 
 async def get_telegram_file(telegram_url: str, video_id: str, file_type: str) -> str:
     logger = LOGGER("StrangerAPI/Youtube.py")
