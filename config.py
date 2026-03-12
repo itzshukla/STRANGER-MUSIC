4534cb15687b3e23e29ab342a14d0767970f72f4
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2026-03-12 15:10:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,8 +50,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ITSZSHUKLA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MASTIWITHFRIENDSXD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sxypndu")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+TQnGqdM7981lNTI9")
 
 # YT-API Get from @AmShashank on tg
 YOUR_API_KEY = getenv("YOUR_API_KEY", "stranger")
